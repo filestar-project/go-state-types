d54b189b0534ff0ce36586ca095e4a5aa3c480e6
codefather <codefather@filestar.net>
2020-12-20 11:05:53 +0800

    update network version

--- network/version.go
+++ network/version.go
@@ -7,13 +7,17 @@ import "math"
 type Version uint
 
 const (
-	Version0 = Version(iota) // genesis   (specs-actors v0.9.3)
-	Version1                 // breeze    (specs-actors v0.9.7)
-	Version2                 // smoke     (specs-actors v0.9.8)
-	Version3                 // ignition  (specs-actors v0.9.11)
-	Version4                 // actors v2 (specs-actors v2.0.3)
-	Version5                 // tape      (specs-actors v2.1.0)
-	Version6                 // upcoming
+	Version0  = Version(iota) // genesis   (specs-actors v0.9.3)
+	Version1                  // breeze    (specs-actors v0.9.7)
+	Version2                  // smoke     (specs-actors v0.9.8)
+	Version3                  // ignition  (specs-actors v0.9.11)
+	Version4                  // actors v2 (specs-actors v2.0.3)
+	Version5                  // tape      (specs-actors v2.1.0)
+	Version6                  // kumquat
+	Version7                  // 8g-sector and fix-iteration-non-determinism)
+	Version8                  // upcoming
+	Version9                  // upcoming
+	Version10                 // upcoming
 
 	// VersionMax is the maximum version number
 	VersionMax = Version(math.MaxUint32)
